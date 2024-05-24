dee347cdd6850e6322bc0844c60c617ce3971dbb
Redamancy-QR <2042204987@qq.com>
2024-05-24 22:35:03 +0800

    End of Chapter 6

--- kernel/main.c
+++ kernel/main.c
@@ -1,4 +1,10 @@
+#include "print.h"
 int main(void){
-    while(1);
+    while(1){
+        put_str("I am kernel!");
+        put_char('\n');
+        put_int(0x66666);
+        put_char('\n');
+    }
     return 0;
 }
